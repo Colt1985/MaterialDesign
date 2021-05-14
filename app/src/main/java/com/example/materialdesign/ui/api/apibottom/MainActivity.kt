68e67e785551ac6245312e8556e2005267a8ccfe
Alex <filippovalex1985@yandex.ru>
2021-05-14 16:18:16 +0300

    Внесены изменения

---
+++ app/src/main/java/com/example/materialdesign/ui/api/apibottom/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.materialdesign.ui.api.apibottom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.materialdesign.R
+import com.example.materialdesign.ui.api.apibottom.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private val NAME_SHARED_PREFERENCE = "LOGIN"
+    private val APP_THEME = "APP_THEME"
+    private val MARS_THEME = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //Установка темы
+        getAppTheme()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+    //Тема из SharedPreferences
+    private fun getAppTheme() {
+        val sharedPreferences = getSharedPreferences(NAME_SHARED_PREFERENCE, MODE_PRIVATE)
+        if (sharedPreferences != null) {
+            val codeStyle = sharedPreferences.getInt(APP_THEME, 0)
+            if (codeStyle == MARS_THEME) {
+                setTheme(R.style.MarsTheme)
+            } else {
+                setTheme(R.style.SpaceTheme)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/materialdesign/ui/api/apibottom/ui/main/MainFragment.kt
@@ -0,0 +1,243 @@
+package com.example.materialdesign.ui.api.apibottom.ui.main
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.BackgroundColorSpan
+import android.text.style.BulletSpan
+import android.view.*
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.transition.ArcMotion
+import androidx.transition.ChangeBounds
+import androidx.transition.TransitionManager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import coil.api.load
+import com.example.materialdesign.R
+import com.example.materialdesign.ui.api.apibottom.MainActivity
+import com.example.materialdesign.ui.api.apibottom.ui.SettingsFragment
+import com.example.materialdesign.ui.api.apibottom.ui.recycler_view.RecyclerActivity
+import com.example.materialdesign.ui.api.apibottom.ui.viewpager.Date
+import com.example.materialdesign.ui.api.apibottom.ui.viewpager.ViewPagerAdapter
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.main_fragment.*
+
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var bsTittle: TextView
+    private lateinit var bsContent: TextView
+    private lateinit var textDate: TextView
+    private var animationPosition = 2
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Добавляем адаптер
+        view_pager.adapter = ViewPagerAdapter(childFragmentManager)
+        setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        setBottomAppBar(view)
+        input_layout.setEndIconOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://en.wikipedia.org/wiki/${input_edit_text.text.toString()}")
+            })
+        }
+        bsTittle = view.findViewById(R.id.bottom_sheet_description_header)
+        bsContent = view.findViewById(R.id.bottom_sheet_description)
+        textDate = view.findViewById(R.id.text_view_date)
+    }
+
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        val currentData = Date()
+//        var date = "${currentData.year}-${currentData.month}-${currentData.day}"
+//
+//        //Определяем первональную позицию
+//        view_pager.currentItem = 1
+//        //Добавлчям листенер на свайп ViewPger чтобы обновлять текст в BottomSheetBehavior
+//        view_pager.addOnPageChangeListener(object  : OnPageChangeListener {
+//            override fun onPageSelected(position: Int) {
+//                when(position) {
+//                    0 -> date = "${currentData.year}-${currentData.month}-${currentData.day-1}"
+//                    1 -> date = "${currentData.year}-${currentData.month}-${currentData.day}"
+//                }
+//                viewModel.getData(date).observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+//                textDate.text = date
+//            }
+//
+//            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+//            override fun onPageScrollStateChanged(state: Int) {}
+//        })
+//
+//        viewModel.getData(date).observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+//        textDate.text = date
+//    }
+
+//    private fun renderData(data: PictureOfTheDayData) {
+//        when (data) {
+//            is PictureOfTheDayData.Success -> {
+//                val serverResponseData = data.serverResponseData
+//                val url = serverResponseData.url
+//                if (url.isNullOrEmpty()) {
+//                    //Отображение ошибки
+//                } else {
+//                    bsTittle.text = serverResponseData.title
+//                    bsContent.text = serverResponseData.explanation
+//                }
+//            }
+//            is PictureOfTheDayData.Loading -> {
+//                //Загрузка
+//            }
+//            is PictureOfTheDayData.Error -> {
+//                //Ошибка
+//            }
+//        }
+//    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val currentData = Date()
+
+        var date = currentData.today
+
+        //Определяем первоночальную позицию
+        view_pager.currentItem = 2
+        //Добавлчям листенер на свайп ViewPager
+        view_pager.addOnPageChangeListener(object  : OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                when(position) {
+                    0 -> {
+                        date = currentData.theDayBeforeYesterday
+                        animationPosition = 0
+                    }
+                    1 -> {
+                        date = currentData.yesterday
+                        animationPosition = 1
+                    }
+                    2 -> {
+                        date = currentData.today
+                        animationPosition = 2
+                    }
+                }
+                viewModel.getData(date).observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+
+                textDate.text = date
+
+                val changeBounds = ChangeBounds()
+                changeBounds.setPathMotion(ArcMotion())
+                changeBounds.duration = 500
+                TransitionManager.beginDelayedTransition(animation, changeBounds)
+                val params = textDate.layoutParams as FrameLayout.LayoutParams
+                params.gravity = when(animationPosition) {
+                    0 -> Gravity.START
+                    1 -> Gravity.CENTER
+                    2 -> Gravity.END
+                    else -> Gravity.END
+                }
+                textDate.layoutParams = params
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
+
+        viewModel.getData(date).observe(viewLifecycleOwner, Observer<PictureOfTheDayData> { renderData(it) })
+        textDate.text = date
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
+            is PictureOfTheDayData.Success -> {
+                val serverResponseData = data.serverResponseData
+                val url = serverResponseData.url
+                if (url.isNullOrEmpty()) {
+                    //Отображение ошибки
+                } else {
+
+                    val imageView = view?.findViewById<ImageView>(R.id.image_view_day);
+                    imageView?.load(url) {
+                        lifecycle(this@MainFragment)
+                        error(R.drawable.ic_load_error_vector)
+                        placeholder(R.drawable.ic_load_error_vector)
+                    }
+
+//                    DayFragment.newInstance(url)
+
+                    bsTittle.text = serverResponseData.title
+                    bsContent.text = serverResponseData.explanation
+
+                    //Использование span
+                    val spannable = SpannableString(bsContent.text)
+                    spannable.setSpan(
+                        BulletSpan(20, Color.GRAY, 20), 0, 1,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                    spannable.setSpan(
+                        BackgroundColorSpan(R.color.orange_light), 13, 16,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
+                    bsContent.text = spannable
+                }
+            }
+            is PictureOfTheDayData.Loading -> {
+                //Загрузка
+            }
+            is PictureOfTheDayData.Error -> {
+                //Ошибка
+            }
+        }
+    }
+
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.bottom_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.settings ->
+                activity?.supportFragmentManager?.beginTransaction()
+                    ?.replace(R.id.container, SettingsFragment.newInstance())
+                    ?.addToBackStack(null)
+                    ?.commit()
+            R.id.notes -> activity?.let { startActivity(Intent(it, RecyclerActivity::class.java)) }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
+    }
+}
